53a47c7502a46ca40bc50ecb77df43c42bf3e336
Juan Roldán <juanroldan1989@gmail.com>
2023-01-12 19:35:15 -0300

    API Gateway Account, Integration and Stage added to support Hello Lambda function's endpoint

---
+++ terraform/3-rest-api-gateway-integration-hello-lambda.tf
@@ -0,0 +1,30 @@
+resource "aws_api_gateway_resource" "hello_resource" {
+  path_part   = "hello"
+  parent_id   = aws_api_gateway_rest_api.main.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.main.id
+}
+
+resource "aws_api_gateway_method" "hello_method" {
+  rest_api_id   = aws_api_gateway_rest_api.main.id
+  resource_id   = aws_api_gateway_resource.hello_resource.id
+  http_method   = "GET"
+  authorization = "NONE" # TODO: To be adjusted with Lambda Authorizer reference
+}
+
+resource "aws_api_gateway_integration" "hello_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.main.id
+  resource_id             = aws_api_gateway_resource.hello_resource.id
+  http_method             = aws_api_gateway_method.hello_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.hello.invoke_arn
+}
+
+resource "aws_lambda_permission" "api_gw_hello_lambda" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.hello.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.main.execution_arn}/*/*"
+}
--- terraform/5-outputs.tf
+++ terraform/5-outputs.tf
@@ -1,7 +1,3 @@
-# output "api_base_url_staging" {
-#   value = aws_apigatewayv2_stage.staging.invoke_url
-# }
-
-# output "api_base_url_production" {
-#   value = aws_apigatewayv2_stage.production.invoke_url
-# }
+output "api_base_url_production" {
+  value = aws_api_gateway_stage.production.invoke_url
+}
