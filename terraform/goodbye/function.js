eb5b75b807b8953f0cc9775e287fc0394e366063
Juan Roldán <juanroldan1989@gmail.com>
2023-01-12 20:21:51 -0300

    Goodbye Lambda Function added to implement secure access through Lambda Authorizer
    - README adjusted with instructions
    - Terraform code added for infrastructure to support Goodbye Lambda function
    - `goodbye` endpoint secured

---
+++ terraform/goodbye/function.js
@@ -0,0 +1,15 @@
+exports.handler = async (event) => {
+  console.log('Event: ', event);
+
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+        message: 'Goodbye!'
+    }),
+  };
+
+  return response;
+};
