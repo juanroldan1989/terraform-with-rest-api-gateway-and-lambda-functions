fabe360f04300f5262330c4f907112123789d7ed
Juan Roldán <juanroldan1989@gmail.com>
2023-01-28 13:25:49 -0300

    Terraform resources tagging best practices applied
    - README adjusted with official references and screenshots
    - Core Features section added

--- terraform/0-providers.tf
+++ terraform/0-providers.tf
@@ -25,6 +25,27 @@ terraform {
   required_version = "~> 1.3.6"
 }
 
+locals {
+  terraform-base-tag = "terraform-rest-api-with-lambda"
+}
+
 provider "aws" {
   region = "us-east-1"
+
+  default_tags {
+    tags = {
+      owner       = "Juan Roldan"
+      project     = "REST API with Lambda Functions"
+      cost-center = "API Billing"
+      Name        = "Managed by Terraform"
+      // This regex results in the terraform git
+      // repo name and any sub-directories.
+      // For this repo, terraform-base-path is
+      // terraform-rest-api-with-lambda/default-tags
+      // This tag helps AWS UI users discover what
+      // Terraform git repo and directory to modify
+      terraform-base-path = replace(path.cwd,
+      "/^.*?(${local.terraform-base-tag}\\/)/", "$1")
+    }
+  }
 }
