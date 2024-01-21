9851029641647e1328266db5288e3e4ee12c0495
Juan Roldán <juanroldan1989@gmail.com>
2024-01-21 20:45:24 +0100

    Terraform resources configuration updated based on official docs
    - API V1 Docs S3 bucket AIM policy configuration fixed
    - [deploy]

--- terraform/2-rest-api-gateway-docs.tf
+++ terraform/2-rest-api-gateway-docs.tf
@@ -1,8 +1,25 @@
 data "aws_iam_policy_document" "website_policy" {
   statement {
     actions = [
-      "s3:GetObject"
+      "s3:*"
     ]
+    effect = "Allow"
+    principals {
+      identifiers = ["*"]
+      type        = "AWS"
+    }
+    resources = [
+      "arn:aws:s3:::${random_pet.docs_api_v1_bucket_name.id}",
+      "arn:aws:s3:::${random_pet.docs_api_v1_bucket_name.id}/*"
+    ]
+  }
+  statement {
+    actions = [
+      "s3:GetObject",
+      "s3:GetObjectVersion"
+    ]
+    effect = "Allow"
+    sid    = "PublicReadGetObject"
     principals {
       identifiers = ["*"]
       type        = "AWS"
@@ -11,6 +28,7 @@ data "aws_iam_policy_document" "website_policy" {
       "arn:aws:s3:::${random_pet.docs_api_v1_bucket_name.id}/*"
     ]
   }
+  depends_on = [aws_s3_bucket_public_access_block.example]
 }
 
 resource "random_pet" "docs_api_v1_bucket_name" {
@@ -29,8 +47,26 @@ resource "aws_s3_bucket_policy" "docs_api_v1" {
 }
 
 resource "aws_s3_bucket_acl" "docs_api_v1" {
+  bucket     = aws_s3_bucket.docs_api_v1.id
+  acl        = "public-read"
+  depends_on = [aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership]
+}
+
+resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
   bucket = aws_s3_bucket.docs_api_v1.id
-  acl    = "public-read"
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+  depends_on = [aws_s3_bucket_public_access_block.example]
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.docs_api_v1.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_versioning" "docs_api_v1" {
@@ -45,8 +81,11 @@ resource "aws_s3_bucket_cors_configuration" "docs_api_v1" {
   bucket = aws_s3_bucket.docs_api_v1.id
 
   cors_rule {
-    allowed_methods = ["GET"]
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "HEAD"]
     allowed_origins = ["*"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
   }
 }
 
